436b62566192943e74a7b54790a257163d7ac034
akileshrao <akileshrao19@gmail.com>
2021-01-23 21:20:50 +0530

    Node js bug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 const cors = require('cors')
-const PORT = process.env.PORT | 5000
+const PORT = process.env.PORT || 5000
 const { addUser, getUser, deleteUser, getUsers } = require('./users')
 
 
